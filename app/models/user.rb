0e36fb45ee04dbb1c25384b2cedf1c5f5436fca6
Edgarsar <edgarsargsyan@hotmail.com>
2022-07-11 05:02:16 +0000

    Add validation for firstname and lastname fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+
 	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
 	validates :email, presence: true, uniqueness: true
 end
