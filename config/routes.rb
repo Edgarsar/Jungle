4ef43a33c4bb1574f3ee820fcca5a476367325b8
Edgarsar <edgarsargsyan@hotmail.com>
2022-07-11 15:30:52 +0000

    Add login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,10 +19,13 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :create, :new]
   end
-  # These routes will be for signup. The first renders a form in the browse, the second will 
-  # receive the form and create a user in our database using the data given to us by the user.
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+
+  get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  resources :users, except: [:new]
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
