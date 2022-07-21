8e6f6e6b8671ee0a6a903643cf83ef0e661487db
Edgarsar <edgarsargsyan@hotmail.com>
2022-07-21 22:05:51 +0000

    Define a class method authenticate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
 	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
 	validates :email, presence: true, uniqueness: { case_sensitive: false }
 	validates :password, presence: true, length: { minimum: 5 }
+
+	def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+		if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
