6b48fcaca1321ae1b7bfd11d8a76d1cc9c0b252b
Edgarsar <edgarsargsyan@hotmail.com>
2022-07-21 21:30:09 +0000

    Add Validation for password field to have min length of 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
 	validates :last_name, presence: true
 
 	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
-	validates :email, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: { case_sensitive: false }
+	validates :password, presence: true, length: { minimum: 5 }
 end
