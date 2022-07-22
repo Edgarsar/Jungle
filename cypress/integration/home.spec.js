1948319200d62a499b10b054574507b9db53a751
Edgarsar <edgarsargsyan@hotmail.com>
2022-07-22 18:20:03 +0000

    Create a tests for the home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe("Home Page", () => {
+
+  it("Should visit root", () => {
+    cy.visit("/")
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+   
+  });
